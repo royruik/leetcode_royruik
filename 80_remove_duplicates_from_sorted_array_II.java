dca312abeb9a681fab3b5583e736ec15d7e7d6a2
Roy Rui <yrui053@uottawa.ca>
2024-09-27 16:39:52 -0400

    Update 80_remove_duplicates_from_sorted_array_II.java

--- 80_remove_duplicates_from_sorted_array_II.java
+++ 80_remove_duplicates_from_sorted_array_II.java
@@ -59,3 +59,17 @@ class Solution {
         return n;
     }
 }
+
+//Better Solution
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
+//         // 数组中的一号和二号元素肯定不用删除
+//         int count = 2;
+//         for(int i = 2 ; i < nums.length ; i++) {
+//             if(nums[i] != nums[count-2]) {
+//                 nums[count++] = nums[i];
+//             }
+//         }
+//         return count;
+//     }
+// }
