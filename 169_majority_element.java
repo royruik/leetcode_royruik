5fd3604adcb12ab0068f6660a7c5f38c79a502e8
Roy Rui <yrui053@uottawa.ca>
2024-09-28 23:29:51 -0400

    sept 28

--- 169_majority_element.java
+++ 169_majority_element.java
@@ -22,3 +22,16 @@
  
 
 // Follow-up: Could you solve the problem in linear time and in O(1) space?
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+        for (int num : nums){
+        	if (count == 0) {
+        		candidate = num;
+        	}
+        	count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+}
