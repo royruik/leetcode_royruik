9584e6ebe2f082b66b15aa7d333bb3a157359aa3
Roy Rui <yrui053@uottawa.ca>
2025-02-06 15:12:59 -0500

    feb 6

---
+++ 71_simplify_path.java
@@ -0,0 +1,103 @@
+// You are given an absolute path for a Unix-style file system, which always begins with a slash '/'. Your task is to transform this absolute path into its simplified canonical path.
+
+// The rules of a Unix-style file system are as follows:
+
+// A single period '.' represents the current directory.
+// A double period '..' represents the previous/parent directory.
+// Multiple consecutive slashes such as '//' and '///' are treated as a single slash '/'.
+// Any sequence of periods that does not match the rules above should be treated as a valid directory or file name. For example, '...' and '....' are valid directory or file names.
+// The simplified canonical path should follow these rules:
+
+// The path must start with a single slash '/'.
+// Directories within the path must be separated by exactly one slash '/'.
+// The path must not end with a slash '/', unless it is the root directory.
+// The path must not have any single or double periods ('.' and '..') used to denote current or parent directories.
+// Return the simplified canonical path.
+
+// Example 1:
+
+// Input: path = "/home/"
+
+// Output: "/home"
+
+// Explanation:
+
+// The trailing slash should be removed.
+
+// Example 2:
+
+// Input: path = "/home//foo/"
+
+// Output: "/home/foo"
+
+// Explanation:
+
+// Multiple consecutive slashes are replaced by a single one.
+
+// Example 3:
+
+// Input: path = "/home/user/Documents/../Pictures"
+
+// Output: "/home/user/Pictures"
+
+// Explanation:
+
+// A double period ".." refers to the directory up a level (the parent directory).
+
+// Example 4:
+
+// Input: path = "/../"
+
+// Output: "/"
+
+// Explanation:
+
+// Going one level up from the root directory is not possible.
+
+// Example 5:
+
+// Input: path = "/.../a/../b/c/../d/./"
+
+// Output: "/.../b/d"
+
+// Explanation:
+
+// "..." is a valid name for a directory in this problem.
+
+ 
+
+// Constraints:
+
+// 1 <= path.length <= 3000
+// path consists of English letters, digits, period '.', slash '/' or '_'.
+// path is a valid absolute Unix path.
+class Solution {
+    public String simplifyPath(String path) {
+        String[] names = path.split("/");
+        Deque<String> stack = new ArrayDeque<String>();
+        for (String name : names) {
+            if ("..".equals(name)) {
+                if (!stack.isEmpty()) {
+                    stack.pollLast();
+                }
+            } else if (name.length() > 0 && !".".equals(name)) {
+                stack.offerLast(name);
+            }
+        }
+        StringBuffer ans = new StringBuffer();
+        if (stack.isEmpty()) {
+            ans.append('/');
+        } else {
+            while (!stack.isEmpty()) {
+                ans.append('/');
+                ans.append(stack.pollFirst());
+            }
+        }
+        return ans.toString();
+    }
+}
+
+// 作者：力扣官方题解
+// 链接：https://leetcode.cn/problems/simplify-path/solutions/1193258/jian-hua-lu-jing-by-leetcode-solution-aucq/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
